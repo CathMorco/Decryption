b83d63aeb0d310d84540973c2365812285b04129
unknown <morco.cathleenjan.12192003@gmail.com>
2023-04-15 18:57:16 +0800

    Decryption Program

---
+++ Decryption Pygame Program.py
@@ -0,0 +1,35 @@
+#Imports the pygame module
+
+#Sets the value for the width and height of the display screen
+
+#Creates the display screen
+
+#RGB example values
+
+#The rate of change in colors
+
+#The color directory & its values
+
+#Initialized values for functions
+
+#Draws the text
+
+#Creates the color change
+
+#Combines the color change and draw text into one function
+
+# Initialising pygame
+
+#Asks the user for the statement that will be decrypted
+
+#Converts the following characters: 'a' = *, 'e' = & , 'i' = # , 'o' = + 'u' = !
+
+# Split into words
+
+# Calculate number of words per line
+
+# Divide into 3 lines
+
+#Combines the 3 lines into one list
+
+#Runs the program
